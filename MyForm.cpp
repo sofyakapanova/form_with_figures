22c5c5e548128a72766e554442256535f50b250d
sofyakapanova <105154076+sofyakapanova@users.noreply.github.com>
2024-05-02 01:33:43 +0300

    Create MyForm.cpp

---
+++ MyForm.cpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	MyForm::MyForm form;
+	Application::Run(% form);
+
+}
